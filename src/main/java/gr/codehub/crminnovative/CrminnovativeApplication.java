eb537f2076a5b8ba990b03bb19be404a12712e2d
Panos <panos_vk@hotmail.com>
2020-07-20 17:07:41 +0300

    New commit

--- src/main/java/gr/codehub/crminnovative/CrminnovativeApplication.java
+++ src/main/java/gr/codehub/crminnovative/CrminnovativeApplication.java
@@ -2,8 +2,10 @@ package gr.codehub.crminnovative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class CrminnovativeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/gr/codehub/crminnovative/model/Customer.java
+++ src/main/java/gr/codehub/crminnovative/model/Customer.java
@@ -3,8 +3,18 @@ package gr.codehub.crminnovative.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @Data
 public class Customer {
     private String name;
+    private String address;
+    private int age;
+    private Date date;
+
+
+    public Customer(String thename){
+        this.name = thename;
+    }
 }
