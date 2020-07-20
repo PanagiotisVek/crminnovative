3f7662718844f4f74d9a649ad18d7a4a99b84c06
Panos <panos_vk@hotmail.com>
2020-07-20 15:19:45 +0300

    Initial commit

---
+++ src/main/java/gr/codehub/crminnovative/model/Customer.java
@@ -0,0 +1,10 @@
+package gr.codehub.crminnovative.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Customer {
+    private String name;
+}
